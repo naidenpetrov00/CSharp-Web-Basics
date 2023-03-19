69f8915373e1e85977751275cdc3d8d20770a7af
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-03-19 12:23:02 +0200

    .

--- HttpRequester/HttpRequester/Program.cs
+++ HttpRequester/HttpRequester/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace HttpRequester
 {
+	using System.Net;
+	using System.Net.Sockets;
+
 	public class Program
 	{
 		public static async Task Main(string[] args)
 		{
+			var tcpListener = new TcpListener(IPAddress.Loopback, 1234);
+
+			while (true)
+			{
+				var tcpCLient = tcpListener.AcceptTcpClient();
+				var networkStream = tcpCLient.GetStream();
+
+				// TODO: Use buffer
+				var requestBytes = new byte[100000];
+				var bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
+			}
+		}
+
+		public static async Task HttpRequester()
+		{
+
 			var client = new HttpClient();
 			var responce = await client.GetAsync("https://softuni.bg/");
 			var result = await responce.Content.ReadAsStringAsync();
