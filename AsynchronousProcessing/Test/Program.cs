3122be0970fb1124e7cb1ab1e925e7c0dd22b42e
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-03-23 13:00:32 +0200

    Asynchronous Processing

--- AsynchronousProcessing/Test/Program.cs
+++ AsynchronousProcessing/Test/Program.cs
@@ -6,6 +6,49 @@ namespace Test
 	{
 		public static void Main(string[] args)
 		{
+			var lockObj = new object();
+			decimal money = 0;
+			var thread1 = new Thread(() =>
+			{
+				for (int i = 0; i < 100000; i++)
+				{
+					lock (lockObj)
+					{
+						money++;
+					}
+				}
+			});
+			thread1.Start();
+
+			var thread2 = new Thread(() =>
+			{
+				for (int i = 0; i < 100000; i++)
+				{
+					lock (lockObj)
+					{
+						money++;
+					}
+				}
+			});
+			thread2.Start();
+
+			thread1.Join();
+			thread2.Join();
+
+			Console.WriteLine(money);
+
+			//var thread = new Thread(MyThreadMainMethod);
+			//thread.Start();
+
+			//while (true)
+			//{
+			//	var line = Console.ReadLine();
+			//	Console.WriteLine(line.ToUpper());
+			//}
+		}
+
+		private static void MyThreadMainMethod()
+		{
 			var sw = Stopwatch.StartNew();
 			Console.WriteLine(CountPrimeNumbers(1, 10000000));
 			Console.WriteLine(sw.Elapsed);
