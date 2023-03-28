6df65969a49c1c6163f527e042902bc3c7ad1831
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-03-28 20:35:00 +0300

    -

--- HttpRequester/HttpRequester/Program.cs
+++ HttpRequester/HttpRequester/Program.cs
@@ -4,6 +4,7 @@
 	using System.Text;
 	using System.Net.Sockets;
 	using System.Net.Http;
+	using System.Text.RegularExpressions;
 
 	public class Program
 	{
@@ -32,18 +33,14 @@
 			var requestBytes = new byte[100000];
 			var bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
 			var request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
-
-			var responseText = @"<form action='/Account/Login' method='post'>
-<input type=text name='username'/>
-<input type=password name='password'/>
-<input type=submit value='Login'/>
-</form>";
-
+			var username = Regex.Match(request, @"Cookie: user=[^\n]*\n").Value;
+			var responseText = "<h1>" + username + "</h1>";
 			var response = "HTTP/1.0 200 OK" + NewLine +
 						"Server: NaidenServer/1.0" + NewLine +
 						"Content-Type: text/html" + NewLine +
 						//"Content-Disposition: attachment; filename=naiden.html" + NewLine +
 						"Content-Lenght: " + responseText.Length + NewLine +
+						"Set-Cookie: user=naiden; Max-Age=3600" + NewLine +
 						NewLine +
 						responseText;
 			var responseBytes = Encoding.UTF8.GetBytes(response);
