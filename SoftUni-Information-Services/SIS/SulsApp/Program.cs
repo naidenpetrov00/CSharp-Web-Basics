a35f7301d057ab7cb251507f2d8a8c591eb77eec
Naiden Nikolaev Petrov <naiden.petrov.31.12.00@gmail.com>
2023-04-10 20:50:31 +0300

    .

--- SoftUni-Information-Services/SIS/SulsApp/Program.cs
+++ SoftUni-Information-Services/SIS/SulsApp/Program.cs
@@ -1,30 +1,12 @@
 ﻿namespace SulsApp
 {
-	using DemoApp;
-	using SIS.HTTP;
 	using SIS.MvcFramework;
-	using SulsApp.Controllers;
-	using System.Collections.Generic;
-	using System.Runtime.CompilerServices;
 
-	public class Program : IMvcApplication
+	public class Program
 	{
 		public static async Task Main()
 		{
-			await WebHost.Start(new Program());
-		}
-
-		public void Configure(IList<Route> routeTable)
-		{
-			routeTable.Add(new Route(HttpMethodType.GET, "/", new HomeController().Index));
-			routeTable.Add(new Route(HttpMethodType.GET, "/Users/Login", new UsersController().Login));
-			routeTable.Add(new Route(HttpMethodType.GET, "/Users/Register", new UsersController().Register));
-		}
-
-		public void ConfigureServices()
-		{
-			var db = new ApplicationDbContext();
-			db.Database.EnsureCreated();
+			await WebHost.Start(new ServiceConfig());
 		}
 	}
 }
---
+++ SoftUni-Information-Services/SIS/SulsApp/ServiceConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace SulsApp
+{
+	using DemoApp;
+	using SIS.HTTP;
+	using SIS.MvcFramework;
+	using SulsApp.Controllers;
+
+	public class ServiceConfig : IMvcApplication
+	{
+
+		public void Configure(IList<Route> routeTable)
+		{
+			routeTable.Add(new Route(HttpMethodType.GET, "/", new HomeController().Index));
+			routeTable.Add(new Route(HttpMethodType.GET, "/Users/Login", new UsersController().Login));
+			routeTable.Add(new Route(HttpMethodType.GET, "/Users/Register", new UsersController().Register));
+		}
+
+		public void ConfigureServices()
+		{
+			var db = new ApplicationDbContext();
+			db.Database.EnsureCreated();
+		}
+	}
+}
